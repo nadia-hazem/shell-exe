72d274488d0c195e08f93de39c1c40f0298d5d20
nadia-hazem <nadia.hazem@laplateforme.io>
2022-09-29 15:57:10 +0200

    Journal-userlogs

---
+++ job8/get_logs.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+cd /home/nedh/shell-exe/job8
+
+rm number_connection-*
+last -f /var/log/wtmp | grep $USER | wc -l > number_connection-`date +%d-%m-%Y-%H-%M`
+
+tar -uvf Backup/log.tar number_connection-*
+
+
+
